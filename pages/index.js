b488457ba0491070deb58276b2b596b9781b4434
juliotorres8k <jcalbuquerquetorres08@gmail.com>
2026-01-30 18:54:30 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
 function Home ()   {
-    return <h1>test test test test test </h1>
+    return <h1> 
+         const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+    const mensagem = document.getElementById("mensagem");
+
+    if (usuario === "admin" && senha === "1234") {
+        localStorage.setItem("logado", "true");
+        window.location.href = "home.html";
+    </h1>
 }
 export default Home;
